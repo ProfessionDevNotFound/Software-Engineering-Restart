b0f8706b8809ec78778fb416b284d7dceac7d097
ProfessionDevNotFound <rohitkumarguleria223@gmail.com>
2026-08-26 23:13:52 +0530

    Day3 : Removed duplicate code

--- Week 1/Classes.py
+++ Week 1/Classes.py
@@ -1,64 +1,6 @@
 #This Program is to learn Abour classes
 #Class is a blueprint of an object u can also think of it as a userdefined DataType
 #Instead of fixed type of data you can add using defined datatypes
-
-class Character:
-    def __init__(self,name,type,health=100,level=0,attackPower=10):
-        self.name=name
-        self._health=health
-        self._level=level
-        self.type=type
-        self.attackPower=attackPower
-    def isAlive(self):
-        if (self._health>0):
-            return True    
-        return False
-    def attack(self,attack,Enemy):
-            Enemy.take_damage(self.attackPower)
-            print(attack," ")
-    def take_damage(self,amount):
-        self._health-=amount
-
-    def heal(self,amount):
-        self._health+=amount
-
-    def display_status(self):
-        print(f'''
-        Hi there my name is {self.name}
-        I am a {self.type}
-        My health is {self._health}
-
-              ''')
-class Mage(Character):
-    def __init__(self, name, health=100, level=0,attackPower=50):
-        super().__init__(name, "Mage", health, level,attackPower)
-    def attack(self,Enemy):
-        if(super().isAlive):
-            return super().attack("Arise",Enemy)
-        else:
-            print("You are dead")
-
-
-class Warrior(Character):
-    def __init__(self, name, health=200, level=0,attackPower=70):
-        super().__init__(name, "Warrior", health, level,attackPower)
-    def attack(self,Enemy):
-        return super().attack("Gomu Gomu no Dawn Whip",Enemy)
-
-class Swordsman(Character):
-    def __init__(self, name, health=150, level=0,attackPower=65):
-        super().__init__(name, "Swordsmen", health, level,attackPower)
-    def attack(self,Enemy):
-        return super().attack("Getsuga Tenso",Enemy)
-
-class Archer(Character): 
-    def __init__(self, name, health=100, level=0,attackPower=55):
-        super().__init__(name, "Archer", health, level,attackPower)
-    def attack(self,Enemy):
-        return super().attack("Piercing Arrow",Enemy)
-
-
-
 class Character:
     def __init__(self, name, type, health=100, level=0, attackPower=10):
         self.name = name
